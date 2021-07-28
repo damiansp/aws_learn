9d490dcfd8f23efeeb5be6e5d102d76762dbc3e1
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-07-28 06:35:06 -0700

    init BERT stuff

--- DS/dev/push_to_s3.sh
+++ DS/dev/push_to_s3.sh
@@ -9,5 +9,6 @@ AWS_PATH=$2
 PROFILE=cbtn-dev
 BUCKET=s3://cbtn-dev-data-analytics
 
-aws --profile $PROFILE s3 cp $LOCAL_PATH $BUCKET/$AWS_PATH
+aws --profile $PROFILE s3 cp $LOCAL_PATH \
+    $BUCKET/aws_datasci_learning/coursera/$AWS_PATH
 
